02edd2be066a841be6180de5839113e20bc09bcf
Chetanmilmile <107764112+Chetanmilmile@users.noreply.github.com>
2022-10-18 18:34:30 +0530

    git commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import Header from './header/Header'
+import Textarea from './textarea/Textarea'
+import Footer from './footer/Footer'
+
+
+function App() {
+
+
+  return (
+
+    
+    <div className='canvas'>
+        <Header/>
+        <Textarea />
+        <Footer />
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/context/GameMode.js
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from "react";
+
+const GameModeContext = createContext();
+
+export const GameModeContextProvider = ({children}) => {
+    
+    const[gameTime, setGameTime] = useState(5);
+
+    const values = {
+        gameTime,
+        setGameTime
+    }
+
+    return (<GameModeContext.Provider value = {values}>{children}</GameModeContext.Provider>)
+
+}
+
+export const useGameMode = () => useContext(GameModeContext);
